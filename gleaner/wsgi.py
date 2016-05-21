5b002860210b1affcf1df1071f4f4d9fdf36461a
jimmcgaw <jpmcgaw@gmail.com>
2016-05-21 07:15:00 -0700

    update wsgi

--- gleaner/wsgi.py
+++ gleaner/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
 import site
 
 site.addsitedir('~/.virtualenvs/gleaner/local/lib/python2.7/site-packages')
+parent_dir = os.path.abspath(os.path.join(yourpath, os.pardir))
+sys.path.append(parent_dir)
+
+# Activate your virtual env
+activate_env=os.path.expanduser("~/.virtualenvs/gleaner/bin/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
 
 from django.core.wsgi import get_wsgi_application
 
